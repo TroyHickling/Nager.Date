07402e0d33cc603e6396becc93d0692efc8b9a14
Troy Hickling <19172035+TroyHickling@users.noreply.github.com>
2022-01-05 16:24:06 +0000

    Add IOM 2022 changes

--- src/Nager.Date/PublicHolidays/IsleOfManProvider.cs
+++ src/Nager.Date/PublicHolidays/IsleOfManProvider.cs
@@ -28,23 +28,70 @@ namespace Nager.Date.PublicHolidays
             var countryCode = CountryCode.IM;
 
             var firstMondayInMay = DateSystem.FindDay(year, Month.May, DayOfWeek.Monday, Occurrence.First);
-            var lastMondayInMay = DateSystem.FindLastDay(year, Month.May, DayOfWeek.Monday);
             var secondFridayInJune = DateSystem.FindDay(year, Month.June, DayOfWeek.Friday, Occurrence.Second);
             var lastMondayInAugust = DateSystem.FindLastDay(year, Month.August, DayOfWeek.Monday);
 
             var items = new List<PublicHoliday>();
-            items.Add(new PublicHoliday(year, 1, 1, "New Year's Day", "New Year's Day", countryCode));
+
+            #region New Year's Day with fallback
+
+            var newYearDay = new DateTime(year, 1, 1).Shift(saturday => saturday.AddDays(2), sunday => sunday.AddDays(1));
+            items.Add(new PublicHoliday(newYearDay, "New Year's Day", "New Year's Day", countryCode));
+
+            #endregion
+
             items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode));
             items.Add(this._catholicProvider.EasterMonday("Easter Monday", year, countryCode));
             items.Add(new PublicHoliday(firstMondayInMay, "Labour Day", "Labour Day", countryCode));
-            items.Add(new PublicHoliday(lastMondayInMay, "Spring Bank Holiday", "Spring Bank Holiday", countryCode));
+
+            var springBankHoliday = this.GetSpringBankHoliday(year, countryCode);
+            if (springBankHoliday != null)
+            {
+                items.Add(springBankHoliday);
+            }
+
+            var queensPlatinumJubilee = this.GetQueensPlatinumJubilee(year, countryCode);
+            if (queensPlatinumJubilee != null)
+            {
+                items.Add(queensPlatinumJubilee);
+            }
+
             items.Add(new PublicHoliday(secondFridayInJune, "Senior Race Day", "Senior Race Day", countryCode));
             items.Add(new PublicHoliday(year, 7, 5, "Tynwald Day", "Tynwald Day", countryCode));
             items.Add(new PublicHoliday(lastMondayInAugust, "Late Summer Bank Holiday", "Late Summer Bank Holiday", countryCode));
-            items.Add(new PublicHoliday(year, 12, 25, "Christmas Day", "Christmas Day", countryCode));
-            items.Add(new PublicHoliday(year, 12, 26, "Boxing Day", "St. Stephen's Day", countryCode));
+
+            var christmasDay = new DateTime(year, 12, 25).Shift(saturday => saturday.AddDays(2), sunday => sunday.AddDays(2));
+            items.Add(new PublicHoliday(christmasDay, "Christmas Day", "Christmas Day", countryCode));
+
+            var sanktStehpenDay = new DateTime(year, 12, 26).Shift(saturday => saturday.AddDays(2), sunday => sunday.AddDays(2));
+            items.Add(new PublicHoliday(sanktStehpenDay, "Boxing Day", "St. Stephen's Day", countryCode));
 
             return items.OrderBy(o => o.Date);
+        }
+
+
+
+        private PublicHoliday GetQueensPlatinumJubilee(int year, CountryCode countryCode)
+        { 
+            var name = "Spring Bank Holiday";
+
+            if (year == 2022)
+            {
+                return new PublicHoliday(year, 6, 2, name, name, countryCode);
+            }
+
+            var lastMondayInMay = DateSystem.FindLastDay(year, Month.May, DayOfWeek.Monday);
+            return new PublicHoliday(lastMondayInMay, name, name, countryCode);
+        }
+
+        private PublicHoliday GetQueensPlatinumJubilee(int year, CountryCode countryCode)
+        {
+            if (year == 2022)
+            {
+                return new PublicHoliday(year, 6, 3, "Queen’s Platinum Jubilee", "Queen’s Platinum Jubilee", countryCode);
+            }
+
+            return null;
         }
 
         ///<inheritdoc/>
@@ -53,6 +100,7 @@ namespace Nager.Date.PublicHolidays
             return new string[]
             {
                 "https://en.wikipedia.org/wiki/Public_holidays_in_the_Isle_of_Man",
+                "https://www.gov.im/categories/home-and-neighbourhood/bank-holidays/"
             };
         }
     }
